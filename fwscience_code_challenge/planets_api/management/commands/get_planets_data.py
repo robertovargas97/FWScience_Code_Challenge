15c6738ce89047ee8c62359fff65b20ba9038afa
Roberto Rojas <roberto.rojas@ivrtechnology.com>
2023-08-24 12:10:58 -0600

    Code Challenge - Adds the model for the planets     - Generates migration - Adds a django command to retrieve the planets data and insert it into the db

--- fwscience_code_challenge/planets_api/management/commands/get_planets_data.py
+++ fwscience_code_challenge/planets_api/management/commands/get_planets_data.py
@@ -1,4 +1,7 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+
+import requests
+from planets_api.models import Planet
 
 
 class Command(BaseCommand):
@@ -7,6 +10,39 @@ class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def handle(self, *args, **kwargs):
+    # We use a generator in case that we get a huge list (maybe this is not the case, however it is better safe than sorry)
+    def _fetch_planets_data_generator(self):
+        """Gets the planets data according to the query defined in the request payload.
 
-        print("--- The command worked ---")
+        Yields:
+            planet (dict): dictionary that contains the planet information (name, population, terrains, climates)
+        """
+        planets_api_url = "https://swapi-graphql.netlify.app/.netlify/functions/index"
+        request_headers = {"Content-Type": "application/json"}
+        request_payload = {
+            "query": "query Query {allPlanets{planets{name population terrains climates}}}"
+        }
+
+        response = requests.post(
+            url=planets_api_url, headers=request_headers, json=request_payload
+        )
+        response.raise_for_status()
+        planets_list = (
+            response.json().get("data", {}).get("allPlanets", {}).get("planets", [])
+        )
+
+        for planet in planets_list:
+            yield planet
+
+    def handle(self, *args, **kwargs):
+        try:
+            for planet in self._fetch_planets_data_generator():
+                new_planet = Planet(**planet)
+                new_planet.save()
+
+            self.stdout.write(self.style.SUCCESS("Planets inserted successfully."))
+
+        except Exception as e:
+            raise CommandError(
+                f"There was an error while executing the command -> {str(e)}"
+            )
--- fwscience_code_challenge/planets_api/models.py
+++ fwscience_code_challenge/planets_api/models.py
@@ -1,3 +1,37 @@
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 
-# Create your models here.
+
+class Planet(models.Model):
+    # Here I did not make the field `name` unique, since I was not sure if for the challenge purposes we want to be able to insert planets with the same name, but different population, terrains, etc (for some reason)
+    name = models.CharField(max_length=250, help_text="The name of the planet")
+    population = models.BigIntegerField(null=True)
+    _terrains = models.TextField(null=True, db_column="terrains")
+    _climates = models.TextField(null=True, db_column="climates")
+
+    # The following properties will allow to manage the terrains and climates as actual lists when interacting with the models, we do not use JSONField, since it is not supported by all the databases engines
+    @property
+    def terrains(self):
+        if self._terrains:
+            return json.loads(self._terrains)
+
+    @terrains.setter
+    def terrains(self, new_value):
+        if new_value:
+            self._terrains = json.dumps(new_value, cls=DjangoJSONEncoder)
+        else:
+            self._terrains = None
+
+    @property
+    def climates(self):
+        if self._climates:
+            return json.loads(self._climates)
+
+    @climates.setter
+    def climates(self, new_value):
+        if new_value:
+            self._climates = json.dumps(new_value, cls=DjangoJSONEncoder)
+        else:
+            self._climates = None
